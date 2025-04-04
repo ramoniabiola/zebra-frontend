066a4e3297ed7e0356333b21cba3bca7d3027db6
ramoni61 <ramoniabiola61@gmail.com>
2025-04-04 12:41:37 +0100

    initial frontend-code commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className="text-slate-800">
+       <h1 className='font-heading'>hello world</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          sans: ['DM Sans', 'sans-serif'],
+          heading: ['Boldonse', 'serif']
+        },
+      },
+    },
+  }
